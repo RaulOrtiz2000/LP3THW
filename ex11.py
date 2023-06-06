32c15df3cf313ca7589f9ed02bc51b799f528b14
RaulOrtiz2000 <130586483+RaulOrtiz2000@users.noreply.github.com>
2023-06-06 15:12:37 -0600

    Add files via upload

--- ex11.py
+++ ex11.py
@@ -1,7 +1,8 @@
-print("Cuantos años tienes?", end=' ')
-age = input()
-print("Cuanto mides?", end=' ')
-height = input()
-print("Cuanto pesas?", end=' ')
-weight = input()
-print(f"Entonces tienes {age} años, estatura, {height} y pesas {weight} Kg.")
+from sys import argv 
+# read the WYSS section for to run this
+script, first, second, third = argv
+
+print("El codigo llamado:", script)
+print("Tu primera variable es", first)
+print("Tu segunda variable es:", second)
+print("Tu tercera variable es:", third)
